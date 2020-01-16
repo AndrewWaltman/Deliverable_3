61b9cb98fbdc18ca73a787679d4e367e3efce75c
AndrewWaltman <andrew.w.waltman@gmail.com>
2020-01-16 16:19:05 -0500

    Updated material for turn in.

--- Deliverable_3/Program.cs
+++ Deliverable_3/Program.cs
@@ -16,13 +16,13 @@ namespace Deliverable_3
             Console.WriteLine("Hey thanks. Now, please insert a number between 1 and 100: ");           
             int number = int.Parse(Console.ReadLine());
 
-            if (number % 2 == 1)
+            if ((number % 2 == 1) && (number < 61))
             {
                 Console.WriteLine("Well, "+ name +". Your number is " + number + ", which is an odd number.");
             }
             else if ((number % 2 == 1) && (number >= 61))
             {
-                Console.WriteLine("Well, " + name + ". Your number is " + number + "which is odd, and greater than 60.");
+                Console.WriteLine("Well, " + name + ". Your number is " + number + ", which is odd, and greater than 60.");
             }
 
             if ((number % 2 == 0) && (number >= 2 && number <= 25 ))
